c3cbb8c4b9dabbbfb76cf6bc1c5800e05e32669e
Matt Ching <matthewaching@gmail.com>
2026-01-15 15:12:33 -0600

    Test with hardcode

--- ssr-example/src/pages/api/updateRabbitNote.ts
+++ ssr-example/src/pages/api/updateRabbitNote.ts
@@ -8,7 +8,8 @@ export default async function handler(
 ) {
     const sql = neon(`${process.env.DATABASE_URL}`, { arrayMode: false });
     if (req.method === 'POST') {
-        await Promise.all(req.body.notes.map((note: Note) => sql.query('UPDATE RabbitNote SET note = $1 WHERE id = $2', [note.note, note.id])));
+        // await Promise.all(req.body.notes.map((note: Note) => sql.query('UPDATE RabbitNote SET note = $1 WHERE id = $2', [note.note, note.id])));
+        await sql.query("UPDATE RabbitNote SET note = 'Loves belly rubs' WHERE id = 1");
     }
     res.status(200);
 }
