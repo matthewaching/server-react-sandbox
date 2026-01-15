6ed93892d17505c5df19bc465fef8ad6e6afa712
Matt Ching <matthewaching@gmail.com>
2026-01-15 14:34:02 -0600

    Update note interface

--- ssr-example/src/pages/api/updateRabbitNote.ts
+++ ssr-example/src/pages/api/updateRabbitNote.ts
@@ -7,6 +7,8 @@ export default async function handler(
     res: NextApiResponse<Note[]>,
 ) {
     const sql = neon(`${process.env.DATABASE_URL}`, { arrayMode: false });
-    await sql.query('UPDATE RabbitNote SET note = $1 WHERE id = $2', [req.body.note, req.body.id]);
+    req.body.notes.forEach(async (note: Note) => {
+        await sql.query('UPDATE RabbitNote SET note = $1 WHERE id = $2', [note.note, note.id]);
+    })
     res.status(200);
 }
