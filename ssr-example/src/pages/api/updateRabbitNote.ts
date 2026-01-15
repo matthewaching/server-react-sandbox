fb967ccb1bbd0db22be19dcf6d448c02d31994df
Matt Ching <matthewaching@gmail.com>
2026-01-15 15:18:10 -0600

    Try adding response body

--- ssr-example/src/pages/api/updateRabbitNote.ts
+++ ssr-example/src/pages/api/updateRabbitNote.ts
@@ -4,12 +4,14 @@ import type { Note } from './getRabbitById/[rabbitId]';
 
 export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse<Note[]>,
+    res: NextApiResponse<any>,
 ) {
     const sql = neon(`${process.env.DATABASE_URL}`, { arrayMode: false });
+
+    let neonResponse = null;
     if (req.method === 'POST') {
         // await Promise.all(req.body.notes.map((note: Note) => sql.query('UPDATE RabbitNote SET note = $1 WHERE id = $2', [note.note, note.id])));
-        await sql.query("UPDATE RabbitNote SET note = 'Loves belly rubs' WHERE id = 1");
+        neonResponse = await sql.query("UPDATE RabbitNote SET note = 'Loves belly rubs' WHERE id = 1");
     }
-    res.status(200);
+    res.status(200).json({ message: neonResponse });
 }
