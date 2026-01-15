c35e3e3679179052ae2bc591a454ee6741d10db8
Matt Ching <matthewaching@gmail.com>
2026-01-15 13:39:19 -0600

    Add breed and fix imgurl

--- ssr-example/src/pages/api/getRabbitById/[rabbitId].ts
+++ ssr-example/src/pages/api/getRabbitById/[rabbitId].ts
@@ -19,9 +19,10 @@ export default async function handler(
     const preferenceResponse = await sql.query('SELECT * FROM RabbitPreference WHERE rabbitId = $1', [rabbitId], { arrayMode: false });
     const response = {
         id: rabbitResponse[0].id,
+        breed: rabbitResponse[0].breed,
         name: rabbitResponse[0].name,
         age: rabbitResponse[0].age,
-        imgUrl: rabbitResponse[0].imgUrl,
+        imgUrl: rabbitResponse[0].imgurl,
         preferences: preferenceResponse.filter(pref => pref.preference).map(pref => pref.food),
     };
     res.status(200).json(response);
