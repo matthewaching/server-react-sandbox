c8ecb04acf7d71100a828b70eb67dfb6527799df
Matt Ching <matthewaching@gmail.com>
2026-01-15 13:01:38 -0600

    Add preference fetch

--- ssr-example/src/pages/api/getRabbitById.ts
+++ ssr-example/src/pages/api/getRabbitById.ts
@@ -2,17 +2,21 @@ import { neon } from '@neondatabase/serverless';
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-    catUrl?: string;
-    subtitle?: string;
-    errorMessage?: string;
+    id: string;
+    name: string;
+    age: number;
+    imgUrl: string;
+    preferences: string[];
 };
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>,
 ) {
-    const sql = neon(`${process.env.DATABASE_URL}`, { arrayMode: false });
+    const sql = neon(`${process.env.DATABASE_URL}`);
     const rabbitId = req.body.rabbitId;
-    const sqlResponse = await sql.query('SELECT * FROM Rabbit WHERE id = $1', [rabbitId]);
-    res.status(200).json(sqlResponse);
+    const rabbitResponse = await sql.query('SELECT * FROM Rabbit WHERE id = $1', [rabbitId], { arrayMode: false });
+    const preferenceResponse = await sql.query('SELECT * FROM RabbitPreference WHERE rabbitId = $1', [rabbitId], { arrayMode: false });
+    rabbitResponse[0].preferences = preferenceResponse.filter(pref => pref.preference).map(pref => pref.food);
+    res.status(200).json(rabbitResponse[0]);
 }
